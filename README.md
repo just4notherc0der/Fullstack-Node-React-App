3f4af62dc90f6254767e1cfdf15a55f5d8c73c3c
just4notherc0der <just4notherc0der@gmail.com>
2017-09-05 09:25:49 -0700

    passport inplemented

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Fullstack-Node-React-App
 following Stephen Grider Udemy course
-
-app URL: https://warm-hamlet-73221.herokuapp.com/
-heroku repo: https://git.heroku.com/warm-hamlet-73221.git
--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
 
 const app = express();
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+  // callback when Google sends user back to /auth/google/callback
+  (accessToken, refreshToken, profile, done) => {
+    // accessToken - the code from Google
+    // refreshToken - undefined
+    // profile - user profile information from Google
+
+    // in this case we dont care about refreshToken. It is used to refresh the access
+    // token because access token exipres after some time
+  })
+);
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${ PORT }`));
